8f1b4d9d747b5d3dd6bc07b62316ba2a7517b1c2
Flre-fly <66842566+Flre-fly@users.noreply.github.com>
2022-02-02 15:10:43 +0900

    upzip complete

--- src/main/java/minji/jplag/controller/MainController.java
+++ src/main/java/minji/jplag/controller/MainController.java
@@ -6,6 +6,7 @@ import minji.jplag.dto.FileDto;
 import minji.jplag.service.FileService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -16,22 +17,27 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
 import java.util.zip.ZipEntry;
+import net.lingala.zip4j.ZipFile;
 import java.util.zip.ZipInputStream;
 
 @Controller
 @Slf4j
 
 public class MainController {
-    @Value("${file.dir}")
-    private String fileDir;
+    //@Value("${file.dir}")
+    //private String fileDir;
+
+
 
     private FileService fileService;
 
-    public MainController(FileService fileService) {
+    public MainController(FileService fileService) throws IOException {
         this.fileService = fileService;
     }
     @GetMapping("upload")
@@ -46,19 +52,33 @@ public class MainController {
                              Model model,
                              HttpServletRequest request) throws IllegalStateException, IOException {
 
+        //String fileDir = new ClassPathResource("/static/files").getFile().getAbsolutePath();
 
-        String fullPath = fileDir + fileName;
-        System.out.println(fullPath);
+        //userdir와 뒤에있는 path를 결합하여 하나의 path로 만드는과정인거임
+        String fileDir = Paths.get(System.getProperty("user.dir"), "src/main/resources/static/files").toString();
+
+        String fullPath = fileDir +"\\"+ fileName;// /아니고 \\임
+        String filePath = fileDir +"\\"+ fileName + ".zip";
         //이거 이름같을경우 예외처리도 해줘야함
         FileDto file = FileDto.builder()
                 .origFilename(uploadfile.getOriginalFilename())
                 .filename(uploadfile.getName())
                 .filePath(fullPath)
                 .build();
+        //압축파일 넘어온것까진 괜찮은데.. 이제..압축파일을 풀어야함
 
-        uploadfile.transferTo(new File(fileDir));
-        System.out.println(uploadfile.getResource().getFile().getAbsolutePath());
-        extractZipFiles(uploadfile.getResource().getFile().getAbsolutePath(),fullPath);
+
+        //String despath = new ClassPathResource(fullPath).getFile().getAbsolutePath();
+        //압축파일이 multipartfile의 경로..
+        String beforeFilePath = fileDir +"\\"+"before.zip";
+        File beforeZipFile = new File(beforeFilePath);
+        uploadfile.transferTo(new File(beforeFilePath));
+
+        extractZipFiles(beforeZipFile,fileDir);
+
+        beforeZipFile.delete();
+
+        //uploadfile.transferTo(new File(fullPath));//fileDir에 파일 저장, 파일에 대한 경로는 fullPath
         fileService.saveFile(file);
 
         model.addAttribute("file", file);//files로 추가했으니 uploadResult.html에거 files로 찾을 수 있어
@@ -67,78 +87,11 @@ public class MainController {
         return "uploadResult";
     }
 
-    public static boolean extractZipFiles(String zip_file, String directory) {
-        boolean result = false;
+    public static void extractZipFiles(File source, String targetDir) throws IOException {
 
-        byte[] data = new byte[2048];
-        ZipEntry entry = null;
-        ZipInputStream zipstream = null;
-        FileOutputStream out = null;
+        ZipFile zipFile = new ZipFile(source);
+        zipFile.extractAll(targetDir);
 
-        if (!(directory.charAt(directory.length() - 1) == '/'))
-            directory += "/";
-
-        File destDir = new File(directory);
-        boolean isDirExists = destDir.exists();
-        boolean isDirMake = destDir.mkdirs();
-
-        try {
-            zipstream = new ZipInputStream(new FileInputStream(zip_file));
-
-            while ((entry = zipstream.getNextEntry()) != null) {
-
-                int read = 0;
-                File entryFile;
-
-                //디렉토리의 경우 폴더를 생성한다.
-                if (entry.isDirectory()) {
-                    File folder = new File(directory+entry.getName());
-                    if(!folder.exists()){
-                        folder.mkdirs();
-                    }
-                    continue;
-                }else {
-                    entryFile = new File(directory + entry.getName());
-                }
-
-                if (!entryFile.exists()) {
-                    boolean isFileMake = entryFile.createNewFile();
-                }
-
-                out = new FileOutputStream(entryFile);
-                while ((read = zipstream.read(data, 0, 2048)) != -1)
-                    out.write(data, 0, read);
-
-                zipstream.closeEntry();
-
-            }
-
-            result = true;
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-            result = false;
-        } catch (IOException e) {
-            e.printStackTrace();
-            result = false;
-        } finally {
-            if (out != null) {
-                try {
-                    out.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            if (zipstream != null) {
-                try {
-                    zipstream.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-
-        return result;
     }
 
 
